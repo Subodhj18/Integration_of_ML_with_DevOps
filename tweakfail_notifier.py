b6a629cf44e5f90f05d3031bc58a3e14d3f829c8
Subodh Jadhav <45094638+Subodhj18@users.noreply.github.com>
2020-05-26 13:08:31 +0530

    Update tweakfail_notifier.py

--- tweakfail_notifier.py
+++ tweakfail_notifier.py
@@ -10,7 +10,7 @@ from email.mime.text import MIMEText
 host_address = "svjadhav1818@gmail.com"
 host_pass = "96618291#@havshya"
 guest_address = "svjadhav7799@gmail.com"
-subject = "Regarding failure of rebuild.py"
+subject = "REGARDING FAILURE OF tweak.py"
 content = """\<html>
   <head></head>
   <body>
