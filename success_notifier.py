a8a22f085afd32b11ff89ac2b4c96eae2e0806ad
Subodh Jadhav <45094638+Subodhj18@users.noreply.github.com>
2020-05-26 12:34:16 +0530

    Update success_notifier.py

--- success_notifier.py
+++ success_notifier.py
@@ -12,16 +12,20 @@ host_address = "svjadhav1818@gmail.com"
 host_pass = "96618291#@havshya"
 guest_address = "svjadhav7799@gmail.com"
 subject = "MODEL HAS ACHIEVED PREDEFINED ACCURACY"
-content = 
-'''Hello, 
-				Developer this is an email regarding to your last commit. Your last commit was taken into consideration and based on that the trained model has given best accuracy .
-				Congratulations on your success.
-			THANK YOU ...'''
+content = """\<html>
+  <head></head>
+  <body>
+    <p style="color: green;"><h2>Hello, 
+		Developer this is an email regarding to success of your model. Congratulations... model has achieved required accuracy.</h2>
+			THANK YOU</p>
+  </body>
+</html>"""
+
 message = MIMEMultipart()
 message['From'] = host_address
 message['To'] = guest_address
 message['Subject'] = subject
-message.attach(MIMEText(content, 'plain'))
+message.attach(MIMEText(content, 'html'))
 session = smtplib.SMTP('smtp.gmail.com', 587)
 session.starttls()
 session.login(host_address, host_pass)
