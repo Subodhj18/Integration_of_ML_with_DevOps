58dcc7a52201646563d21ed2c03fc315a8c0b5bb
Subodh Jadhav <45094638+Subodhj18@users.noreply.github.com>
2020-05-26 12:29:15 +0530

    Update tweakfail_notifier.py

--- tweakfail_notifier.py
+++ tweakfail_notifier.py
@@ -11,14 +11,19 @@ host_address = "svjadhav1818@gmail.com"
 host_pass = "96618291#@havshya"
 guest_address = "svjadhav7799@gmail.com"
 subject = "Regarding failure of rebuild.py"
-content = '''Hello, 
-				Developer this is an email regarding to your last commit. It seems that your tweak.py fileis not working properly please check it once and recommit.
-			THANK YOU'''
+content = """\<html>
+  <head></head>
+  <body>
+    <p style="color: red;"><h2>Hello, 
+		Developer this is an email regarding to your last commit. It seems that your tweak.py file is not working properly please check it once and recommit.</h2>
+			THANK YOU</p>
+  </body>
+</html>"""
 message = MIMEMultipart()
 message['From'] = host_address
 message['To'] = guest_address
 message['Subject'] = subject
-message.attach(MIMEText(content, 'plain'))
+message.attach(MIMEText(content, 'html'))
 session = smtplib.SMTP('smtp.gmail.com', 587)
 session.starttls()
 session.login(host_address, host_pass)
